9db09408e945d093e2961aa60b197e232e6107bb
PawelBialy <PawelBialy@users.noreply.github.com>
2022-11-20 22:07:58 +0100

    solve_it

---
+++ random/new_random.py
@@ -0,0 +1,30 @@
+
+lista = ["jeden", 'dwa', 'trzy', 'cztery', 'maderfaker']
+
+# shoulud_continue =  True
+#ilosc = len(lista)
+# while shoulud_continue :
+#     print(range(ilosc))
+# indeks = 0
+# while indeks < len(lista ) :
+#     print(lista[indeks])
+#     indeks += 1
+
+
+for x in lista :
+    print(x)
+
+print(list(range(-10,10)))
+
+def moja_funkcja() :
+     print('ja jebie')
+moja_funkcja()
+
+def dodaj(x):
+    print(x+1)
+dodaj(4)
+
+def dodaj(x, y):
+    print (x + y)
+dodaj(2,4)
+dodaj(4)
