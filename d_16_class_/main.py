a5dc6e37967ae47dee4160d1d259d07ec75b7f0d
PawelBialy <PawelBialy@users.noreply.github.com>
2022-12-15 10:36:05 +0100

    day_16- start

---
+++ d_16_class_/main.py
@@ -0,0 +1,26 @@
+# from turtle import Turtle, Screen
+#
+# dzony = Turtle()
+# print(dzony)
+# dzony.shape("turtle")
+# dzony.color("blue4")
+# dzony.position()
+# dzony.forward(100)
+# dzony.right(90)
+# dzony.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+#main.py
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column( "Pokemon",["Aipom","Alakazam","Psychic","Pikachu"])
+table.add_column("Type", ["Normal", "Psychic", "Fairy","Elecrtic" ])
+table.
+
+print(table)
+
