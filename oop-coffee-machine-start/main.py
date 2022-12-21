88651e26652b28a20655f90290154aee6fb0ac58
PawelBialy <PawelBialy@users.noreply.github.com>
2022-12-21 11:13:02 +0100

    day 17 - only main.py

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,7 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_machine = MoneyMachine()
+money_machine = MoneyMachine()
 coffe_maker = CoffeeMaker()
 menu = Menu()
 
@@ -15,9 +15,9 @@ while is_on:
         is_on = False
     elif choice == "report":
         coffe_maker.report()
-        my_machine.report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
         if coffe_maker.is_resource_sufficient(drink) :
-           if my_machine.make_payment(drink.cost):
+           if money_machine.make_payment(drink.cost):
                coffe_maker.make_coffee(drink)
--- oop-coffee-machine-start/menu.py
+++ oop-coffee-machine-start/menu.py
@@ -10,6 +10,7 @@ class MenuItem:
         }
 
 
+
 class Menu:
     """Models the Menu with drinks."""
     def __init__(self):
