b3e37a007a3530a0ed95fadbb43e2ef0b993e115
PawelBialy <114645036+PawelBialy@users.noreply.github.com>
2023-01-06 13:22:55 +0100

    Update main.py

--- day-18/main.py
+++ day-18/main.py
@@ -47,7 +47,20 @@ def random_color():
     b = random.randint(0,255)
     random_color = (r,g,b)
     return random_color
+ 
           
+          
+ ####Spirograph 
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap )):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+draw_spirograph(15)
+
+
+
 #######First Version
 # for _ in range(3):
 #     tim.forward(100)
