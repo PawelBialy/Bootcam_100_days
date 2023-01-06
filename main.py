b74f7273cb80a4eba6552502d1a886df10544404
PawelBialy <114645036+PawelBialy@users.noreply.github.com>
2023-01-06 15:25:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 20)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r,g,b)
+    rgb_colors.append(new_color)
+print(rgb_colors)
+
+new_color_list = [ (232, 251, 242), (198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5), (229, 159, 46), (27, 40, 157), (215, 74, 12), (15, 154, 16), (199, 14, 10), (242, 246, 252), (243, 33, 165), (229, 17, 121), (73, 9, 31), (60, 14, 8), (224, 141, 211), (10, 97, 61)]
