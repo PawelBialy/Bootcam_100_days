99f9c5a68bfdd4e09a95d5ce8d5d9f84b667ce1b
PawelBialy <PawelBialy@users.noreply.github.com>
2023-01-20 15:31:20 +0100

    Pong game - day 22

--- day22_pong_game/main.py
+++ day22_pong_game/main.py
@@ -33,4 +33,13 @@ while game_is_on:
     #detect collision with paddle
     if ball.distance(r_paddle ) < 50 and ball.xcor() > 320 or  ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    if ball.xcor() > 350 :
+        ball.reset_position()
+
+    if ball.xcor() < -350:
+        ball.reset_position()
+
+
+
 screen.exitonclick()
