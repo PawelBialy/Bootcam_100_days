d2d661996c57a7db8340412cabfee4351abe6dad
PawelBialy <PawelBialy@users.noreply.github.com>
2023-01-21 19:44:03 +0100

    day_23_cross_game

--- day_23_cross_game/main.py
+++ day_23_cross_game/main.py
@@ -13,6 +13,8 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 game_is_on = True
@@ -24,6 +26,15 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    if player.is_at_finish_line():
+        player.got_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
 
 
 
