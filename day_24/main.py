50b9f4ecb86f4b86e78bb4607ed7633240bc37c8
PawelBialy <PawelBialy@users.noreply.github.com>
2023-01-28 12:25:24 +0100

    day 24

--- day_24/main.py
+++ day_24/main.py
@@ -1,6 +1,12 @@
-# with  open ("my_text.txt") as file:
+### Relative /file Path
+
+# with  open ("../../../../../Desktop/my_text.txt") as file:
 #     contents = file.read()
 #     print(contents)
 
-with open("my_text.txt", mode="a") as file:
-    sample_text = file.write("\nsample text")
+
+
+# Absolute File Path
+with open ("/Users/allec/Desktop/my_text.txt") as file:
+    contents = file.read()
+    print(contents)
