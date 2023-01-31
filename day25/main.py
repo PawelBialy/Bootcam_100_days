e689e5ca8201f5599eb32a708c02dfdb0343abaf
PawelBialy <PawelBialy@users.noreply.github.com>
2023-01-31 19:33:10 +0100

    day_25

--- day25/main.py
+++ day25/main.py
@@ -1,40 +1,61 @@
-
+#
 import pandas
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len (data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len (data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len (data[data["Primary Fur Color"] == "Black"])
 
-data = pandas.read_csv("weather_data.csv")
-# print(type(data))
-
-# # print(data["temp"])
-# data_dict = data.to_dict()
-# #print(data_dict)
-#
-# temp_list = data["temp"].to_list()
-#
-# print (len(temp_list))
-#
-# print(data["temp"].mean())
-# print(data["temp"].max())
-# average = sum(temp_list) / len(temp_list)
-# print(average)
-
-#Get data in columns
-#name_of_variable["name_of_column"].name_of_function !!!
-#print (data["contition"])   = print(data.condition) same way
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
 
-#get data in row
-# print(data[data.temp == data.temp.max()])
-# print(data[data.day == "Monday"])
 
-# monday = data[data.day == "Monday"]
-# monday_temp = int(monday.temp)
-# monday_tempF = ((monday.temp) * 9/5 ) + 32
-# print(monday_tempF)
-
-#create dataframe from scratch
 data_dict = {
-    "students":["Amy", "James", "Angela"],
-    "scores": [ 79, 56, 68]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count,red_squirrels_count,black_squirrels_count]
 }
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrels_count.csv")
+
+# print(data["Primary Fur Color"])
+
+
+# data = pandas.read_csv("weather_data.csv")
+# # print(type(data))
+#
+# # # print(data["temp"])
+# # data_dict = data.to_dict()
+# # #print(data_dict)
+# #
+# # temp_list = data["temp"].to_list()
+# #
+# # print (len(temp_list))
+# #
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+# # average = sum(temp_list) / len(temp_list)
+# # print(average)
+#
+# #Get data in columns
+# #name_of_variable["name_of_column"].name_of_function !!!
+# #print (data["contition"])   = print(data.condition) same way
+#
+#
+# #get data in row
+# # print(data[data.temp == data.temp.max()])
+# # print(data[data.day == "Monday"])
+#
+# # monday = data[data.day == "Monday"]
+# # monday_temp = int(monday.temp)
+# # monday_tempF = ((monday.temp) * 9/5 ) + 32
+# # print(monday_tempF)
+#
+# #create dataframe from scratch
+# data_dict = {
+#     "students":["Amy", "James", "Angela"],
+#     "scores": [ 79, 56, 68]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
