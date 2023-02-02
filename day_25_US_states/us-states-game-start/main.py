8c5e45afa817848b74eacd18aed9997da87b6f2e
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-02 12:30:52 +0100

    day_25_U.S_States_game

--- day_25_US_states/us-states-game-start/main.py
+++ day_25_US_states/us-states-game-start/main.py
@@ -1,17 +1,37 @@
 import turtle
+import pandas
 from turtle import Turtle
-
 
 screen = turtle.Screen()
 
-screen.title("U.S States Game")
+screen.title("U.S.  States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-answer_satate = screen.textinput("Guess the State", prompt="What is another state's name? ")
-print(answer_satate)
+while len (guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guessed", prompt="What is another state's name? ")
+    answer_state = answer_state.title()
+    #print(answer_state)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+
+
+
+
 
 
 turtle.mainloop()
