1691401306b58d623a4252ebf5cf15adafe09ff9
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-06 19:33:27 +0100

    day_27_Tkinter_*args_**Kw

--- day_27/main.py
+++ day_27/main.py
@@ -1,38 +1,33 @@
 from  tkinter import *
 
-window = Tk()
-
-window.title("My firts GUI Program")
-window.minsize(width=500,height=300)
-
-my_label = Label(text="Create it in the middle",font=("Arial",24,"bold"))
-my_label.pack()
-
-my_label.config(text="New text")
-
-#button
 def button_click():
-    # optional
+    print("I got clicked")
     new_text = input.get()
     my_label.config(text=new_text)
 
-    # my_label.config(text=input.get()) --> This is also  working, and for me is simpler.
-
+window = Tk()
+window.title("My firts GUI Program")
+window.minsize(width=500,height=300)
+window.config(padx=20,pady=20)
+#Label
+my_label = Label(text="Create it in the middle",font=("Arial",24,"bold"))
+my_label.config(text="New text")
+my_label.grid(column=0,row=0)
+my_label.config(padx=50, pady=50)
+#Button
 button = Button(text="Click me", command=button_click)
-button.pack()
+button.grid(column=1,row=1)
+
+#New button
+new_button = Button(text="That's new button", command=button_click )
+new_button.grid(column=2, row=0)
+
 
 #Entry
-
 input = Entry(width=10)
-input.pack()
-# input.get()
+print(input.get())
+input.grid(column=3,row=2)
 
-#
-# def add (*args):
-#     for n in args:
-#         print(n)
-#
-# add(3,5,7,9)
 
 
 window.mainloop()
