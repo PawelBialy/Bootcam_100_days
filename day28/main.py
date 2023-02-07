9695e443db0d0de908fa92cfb1e888d21c4ecfad
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-07 22:04:05 +0100

    Day_28_pomodoro

--- day28/main.py
+++ day28/main.py
@@ -7,12 +7,20 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer_time = None
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer ():
+    window.after_cancel(timer_time)
+    timer_label.config(text="Timer")
+    canvas.itemconfig(timer_text,text="00:00")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
@@ -46,10 +54,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000,count_down,count - 1)
+        global timer_time
+        timer_time  = window.after(1000,count_down,count - 1)
     else:
         start_timer()
-
+        marks = ""
+        work_sessions = math.floor((reps/2))
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -63,18 +76,16 @@ timer_text =  canvas.create_text(100,130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1,row=1)
 
 
-
-
 timer_label = Label(text="Timer",font=(FONT_NAME,48,"bold"),bg=YELLOW, fg=GREEN, highlightthickness=0)
 timer_label.grid(column=1, row=0, )
 
 start_button = Button(text="Start",highlightthickness=0,command=start_timer)
 start_button.grid(column=0,row=2)
 
-reset_button = Button(text="Reset",highlightthickness=0)
+reset_button = Button(text="Reset",highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2,row=2)
 
-check_marks = Label(text="✔", font=24,bg=YELLOW, fg=GREEN,highlightthickness=0)
+check_marks = Label( font=24,bg=YELLOW, fg=GREEN,highlightthickness=0)
 check_marks.grid(column=1, row=3)
 
 
