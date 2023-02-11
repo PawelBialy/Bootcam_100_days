7218ea80becc588f01c5fc704067cc3d9a93bfe7
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-11 10:45:47 +0100

    Day_29_Password_manager

--- day_29/main.py
+++ day_29/main.py
@@ -4,6 +4,15 @@ PINK = "#e2979c"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = webside_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("save_data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        webside_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,9 +37,11 @@ password_text.grid(column=0, row=3)
 
 webside_entry = Entry(width=50,)
 webside_entry.grid(column=1,row=1, columnspan=2)
+webside_entry.focus()
 
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0,"adres_email@gmail.com")
 
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3, columnspan=1)
@@ -38,7 +49,7 @@ password_entry.grid(column=1, row=3, columnspan=1)
 new_password = Button(text="Random Password", )
 new_password.grid(column=2,row=3, columnspan=1)
 
-add_button = Button(width=43, text="Add")
+add_button = Button(width=43, text="Add",command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
