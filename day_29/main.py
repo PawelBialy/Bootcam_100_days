8569d3dbe2cc689227d981d1ab5dd565e4985f74
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-12 23:08:40 +0100

    Day_29_Password_manager

--- day_29/main.py
+++ day_29/main.py
@@ -1,8 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import  pyperclip
+
 GREEN = "#9bdeac"
 PINK = "#e2979c"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_numbers + password_letters + password_symbols
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0,password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -52,10 +71,11 @@ email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0,"adres_email@gmail.com")
 
-password_entry = Entry(width=32)
+password_entry = Entry(width=32, )
 password_entry.grid(column=1, row=3, columnspan=1)
 
-new_password = Button(text="Random Password", )
+
+new_password = Button(text="Random Password", command=generate_password )
 new_password.grid(column=2,row=3, columnspan=1)
 
 add_button = Button(width=43, text="Add",command=save)
