954e3bc9f39eb9015c760cd91812010a925204da
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-14 11:47:57 +0100

    Day_29_Password_manager

--- day_29/main.py
+++ day_29/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import  pyperclip
+import json
 
 GREEN = "#9bdeac"
 PINK = "#e2979c"
@@ -28,17 +29,28 @@ def save():
     website = webside_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_dictionary = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
 
-    if len(website) < 3 or len(password) < 3:
-        messagebox.showinfo(title="Hey!", message="Text field must contain at least 3 symbols.")
+
+    if len(website) ==0 or len(password) ==0:
+        messagebox.showinfo(title="Hey!", message="Text field can't be empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail:{email} "
-                                                      f"\nPassword:{password} \nIs it ok to save?")
-        if is_ok:
-             with open("save_data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                webside_entry.delete(0, END)
-                password_entry.delete(0, END)
+        with open("save_data.json", "r") as data_file:
+            # Reading old data
+            data= json.load(data_file)
+            #Updating old data with new data
+            data.update(new_dictionary)
+        with open("save_data.json","w") as data_file:
+            #saving updated data
+            json.dump(data, data_file, indent=4)
+
+            webside_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 
