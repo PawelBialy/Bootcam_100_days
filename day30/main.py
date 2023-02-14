05373c4beb23bbcf9fab22b9e22f5a753a212c58
PawelBialy <PawelBialy@users.noreply.github.com>
2023-02-14 10:51:55 +0100

    day_30_catching_exceptions

--- day30/main.py
+++ day30/main.py
@@ -17,11 +17,50 @@
 #
 # raise
 
-height = float(input("Height: "))
-weight = int(input("Weight: "))
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+#
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 37, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
 
-if height > 3:
-    raise ValueError("Human Height should not be over 3 meters.")
+total_likes = 0
 
-bmi = weight / height ** 2
-print(bmi)
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass
+
+print(total_likes)
+
+
+
+
+
+# if height > 3:
+#     raise ValueError("Human Height should not be over 3 meters.")
+#
+# bmi = weight / height ** 2
+# print(bmi)
+
+
+
+fruits = ["Apple", "Pear", "Orange"]
+
+##: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError :
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+make_pie(4)
+
