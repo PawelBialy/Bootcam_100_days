a6c4615a9636b66f2c1d0dfa1866a81cc298e2bc
PawelBialy <PawelBialy@users.noreply.github.com>
2023-03-09 17:44:06 +0100

    day_39_deal_finder

---
+++ day_39_capstone_project_deal_finder/data_manager.py
@@ -0,0 +1,5 @@
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    pass
+
+    sheet_api = "https://api.sheety.co/cd4cdf720a554ac4e1919d9c8297a553/copyOfFlightDeals/prices"
