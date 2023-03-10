f104a8e88c1501d9cf40cef2f1577e9930a0ba65
PawelBialy <PawelBialy@users.noreply.github.com>
2023-03-10 21:39:53 +0100

    day_39_deal_finder

---
+++ day_39_deal_finder/data_manager.py
@@ -0,0 +1,24 @@
+import requests
+SHEETY_ENDPOINT = "https://api.sheety.co/cd4cdf720a554ac4e1919d9c8297a553/copyOfFlightDeals/prices"
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT,)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination(self):
+        for city in self.destination_data:
+            new_data = {
+                'price' : {
+                "iataCode" : city["iataCode"]
+            }
+            }
+
+            response = requests.put(url=f"{SHEETY_ENDPOINT}/{city['id']}",json=new_data )
+            print(response.text)
---
+++ day_39_deal_finder/flight_search.py
@@ -0,0 +1,5 @@
+class FlightSearch:
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working.
+        code = "TESTING"
+        return code
