1e37b86b6b4dde1900a6307ef801381878850838
PawelBialy <PawelBialy@users.noreply.github.com>
2023-03-12 21:12:28 +0100

    day_39_deal_finder

--- day_39_deal_finder/data_manager.py
+++ day_39_deal_finder/data_manager.py
@@ -1,4 +1,5 @@
 import requests
+# from pprint import pprint/
 SHEETY_ENDPOINT = "https://api.sheety.co/cd4cdf720a554ac4e1919d9c8297a553/copyOfFlightDeals/prices"
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
@@ -10,6 +11,7 @@ class DataManager:
         response = requests.get(url=SHEETY_ENDPOINT,)
         data = response.json()
         self.destination_data = data["prices"]
+        #pprint (data)
         return self.destination_data
 
     def update_destination(self):
--- day_39_deal_finder/flight_data.py
+++ day_39_deal_finder/flight_data.py
@@ -1,3 +1,11 @@
+search_endpoint = "api.tequila.kiwi.com/search"
+search_api_key = "jej1fyqMl6dFbbnQasBMtbpwdxXtAc0R"
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self, price, origin_city, origin_airport, destination_city, our_date, return_date)
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.our_date = our_date
+        self.return_date = return_date
